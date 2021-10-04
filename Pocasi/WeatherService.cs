e63643ebd99ff242366fb7a68f22f2c3929df897
Afareus <afareus89@gmail.com>
2021-10-04 13:12:18 +0200

    přidán projekt Shared (backend) a jeho základní kostra

---
+++ Pocasi/WeatherService.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pocasi {
+    class WeatherService {
+        // http://api.weatherstack.com/current?access_key=da223c0631c80400f3e7ed08a1a33fc9&query=New%20York
+    }
+}
